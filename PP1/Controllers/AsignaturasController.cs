7b84d940e8a7e8631ce0bb31e1e053df78ed733d
Juan Carlos Carrion Disla <109393161+JuanCarrion29@users.noreply.github.com>
2024-10-06 22:13:43 -0400

    Agregar archivos de proyecto.

---
+++ PP1/Controllers/AsignaturasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PP1.Models;
+
+namespace PP1.Controllers
+{
+    public class AsignaturasController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Asegúrate de que la lista esté correctamente inicializada
+            List<Asignatura> asignaturas = new List<Asignatura>
+        {
+            new Asignatura { NombreAsignatura = "Matemáticas II", NombreProfesor = "Dr. Pérez", Creditos = 4, Cuatrimestre = 3 },
+            new Asignatura { NombreAsignatura = "Programación Avanzada", NombreProfesor = "Ing. García", Creditos = 5, Cuatrimestre = 4 },
+            new Asignatura { NombreAsignatura = "Física II", NombreProfesor = "Dr. López", Creditos = 4, Cuatrimestre = 3 },
+            new Asignatura { NombreAsignatura = "Bases de Datos", NombreProfesor = "Lic. Hernández", Creditos = 4, Cuatrimestre = 4 }
+        };
+
+            // Pasa la lista a la vista
+            return View(asignaturas);
+        }
+    }
+
+}
+
---
+++ PP1/Models/Asignatura.cs
@@ -0,0 +1,10 @@
+﻿namespace PP1.Models
+{
+    public class Asignatura
+    {
+        public string NombreAsignatura { get; set; }
+        public string NombreProfesor { get; set; }
+        public int Creditos { get; set; }
+        public int Cuatrimestre { get; set; }
+    }
+}
